1d248ee776c7c4a1e46981c24e3816b7cc8b73fb
kong <70005144+kongyb@users.noreply.github.com>
2023-03-22 00:01:03 +0900

    B1967
    https://www.acmicpc.net/problem/1967

--- Baekjoon/B1967.js
+++ Baekjoon/B1967.js
@@ -1,88 +1,80 @@
-const readline = require('readline');
-const { threadId } = require('worker_threads');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function (line){
-	input.push(line);
-})
-.on('close',function(){
-	//input에 입력값 한줄씩있음
-	let cnt = Number(input[0]);
-	let treeNodes = {};
-	treeNodes[1] = new TreeNode(1);
-	for (let i=1; i<cnt; i++){
-		let [parent, child, dist] = input[i].split(' ').map(el => Number(el));
-		treeNodes[parent].addChild(child, dist, treeNodes);
-	}
-	let max = 0;
-	for (const key in treeNodes){
-		let len = treeNodes[key].center();
-		max = max < len ? len : max;
-	}
-	console.log(max);
-	process.exit();
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  //input에 입력값 한줄씩있음
+  const cnt = Number(input[0]);
+  const nodes = [];
+  for (let i = 0; i <= cnt; i++) {
+    nodes.push(new TreeNode());
+  }
+  for (let i = 1; i < cnt; i++) {
+    const [p, c, len] = input[i].split(" ").map((el) => Number(el));
+    const parentNode = nodes[p];
+    const childNode = nodes[c];
+    parentNode.addChild(childNode, len);
+  }
+  console.log(solution(nodes));
+  process.exit();
 });
 
-class TreeNode{
-	constructor(num){
-		this.num = num;
-		this.child = [];
-		this.dists=[]
-		this.centerMax = 0;
-		this.throughMax = 0;
-	}
+function solution(nodes) {
+  const root = nodes[1];
+  root.calculate();
+  let max = 0;
+  for (let i = 1; i < nodes.length; i++) {
+    max = Math.max(max, nodes[i].getMaxDiameter());
+  }
+  return max;
+}
 
-	addChild(childNum, dist, treeNodes){
-		if (!treeNodes[childNum])
-			treeNodes[childNum] = new TreeNode(childNum);
-		this.child.push(treeNodes[childNum]);
-		this.dists.push(dist);
-	}
+class TreeNode {
+  constructor() {
+    this.child = [];
+    this.dist = [];
+    this.maxDiameter = 0;
+    this.maxPath = 0;
+  }
 
-	center(){
-		if (this.centerMax > 0)
-			return this.centerMax;
-		if (this.child.length === 0)
-			this.centerMax=0;
-		else if (this.child.length === 1)
-			this.centerMax = this.through();
-		else{
-			let index1 = 0;
-			let max = 0;
-			for (let i=0; i<this.child.length; i++){
-				if ((this.dists[i] + this.child[i].through()) > max){
-					index1 = i;
-					max = this.dists[index1] + this.child[index1].through();
-				}
-			}
-			max = 0;
-			for (let i=0; i<this.child.length; i++){
-				if (i === index1)
-					continue;
-				max = max < (this.dists[i] + this.child[i].through()) ? (this.dists[i] + this.child[i].through()) : max;
-			}
-			this.centerMax = max + this.dists[index1] + this.child[index1].through();
-		}
-		return this.centerMax;
-	}
+  addChild(childNode, len) {
+    this.child.push(childNode);
+    this.dist.push(len);
+  }
 
-	through(){
-		if (this.throughMax > 0)
-			return this.throughMax;
-		if (this.child.length === 0)
-			this.throughMax=0;
-		else{
-			let max = 0;
-			for (let i=0; i<this.child.length; i++)
-				max = max < (this.dists[i] + this.child[i].through()) ? (this.dists[i] + this.child[i].through()) : max;
-			this.throughMax = max;
-		}
-		return this.throughMax;
-	}
+  calculate() {
+    if (this.child.length === 0) {
+      return;
+    }
+    const path = [];
+    for (let i = 0; i < this.child.length; i++) {
+      const c = this.child[i];
+      c.calculate();
+      path.push(c.getMaxPath() + this.dist[i]);
+    }
+    if (path.length === 1) {
+      this.maxPath = path[0];
+      this.maxDiameter = path[0];
+      return;
+    }
+    path.sort((a, b) => b - a);
+    this.maxPath = path[0];
+    this.maxDiameter = path[1] + path[0];
+    return;
+  }
+
+  getMaxPath() {
+    return this.maxPath;
+  }
+
+  getMaxDiameter() {
+    return this.maxDiameter;
+  }
 }
